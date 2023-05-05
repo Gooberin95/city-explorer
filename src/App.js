2e90e7655b2bc9b9d430d2274677f40fd75dbf63
Brandon Ruiz <Sober4202020@outlook.com>
2023-05-05 02:33:04 +0000

    getting closer but need to customize away from teachers template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      searchQuery:'',
+      location: {}
+    }
+  }
+
+  getLocation = async () => {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+    
+    const res = await axios.get(API);
+    console.log(res.data);
+    this.setState({ location:res.data[0] });
+    console.log(API);
+  }
+
+  render() {
+    return(
+      <>
+        <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="search for a city"></input>
+        <button onClick={this.getLocation}>Explore!</button>
+        {this.state.location.place_id && 
+          
+          <h2>Latitude is {this.state.location.display_name}</h2>
+        }
+        {this.state.location.place_id && 
+          
+          <h2>Latitude is {this.state.location.lat}</h2>
+        }
+          {this.state.location.place_id && 
+          
+          <h2>Latitude is {this.state.location.lon}</h2>
+        }
+
+      </>
+    )
+  }
 }
 
 export default App;
+
